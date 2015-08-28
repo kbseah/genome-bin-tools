b8348718bb9048d6f1fdcae1e451e4ce9fd60e1d
kbseah <kbseah@users.noreply.github.com>
2015-08-28 13:54:25 +0200

    Fix bug in setOperation() due to empty dataframes

--- gbtools/R/setOperation.gbtbin.R
+++ gbtools/R/setOperation.gbtbin.R
@@ -33,27 +33,36 @@ setOperation.gbtbin <- function(x1,x2,shortlist) {
     marker.tab <- list()  # Make empty list object (NA will throw error later)
     tRNAs.tab <- NA
     ## Combine marker tables #######################################################
-    if (!is.na(x1$markTab) || !is.na(x2$markTab)) {
-        markTab.add <- unique (rbind(x1$markTab,x2$markTab))
-        for (j in 1:length(marksource)) {
-            markTab.subsubset <- subset(markTab.add,source==marksource[j])
-            bin.nummarkers[j] <- dim(markTab.subsubset)[1]
-            marker.tab[[j]] <- table(markTab.subsubset$gene)
-            bin.uniqmarkers[j] <- length(which(marker.tab[[j]] > 0))
-            bin.singlemarkers[j] <- length(which(marker.tab[[j]] ==1))
+    #if (!is.na(x1$markTab) || !is.na(x2$markTab)) {
+    if (is.data.frame(x1$markTab) && is.data.frame(x2$markTab)) {
+        if (dim(x1$markTab)[1] > 0 || dim(x2$markTab)[1] > 0) { 
+            markTab.add <- unique (rbind(x1$markTab,x2$markTab))
+            for (j in 1:length(marksource)) {
+                markTab.subsubset <- subset(markTab.add,source==marksource[j])
+                bin.nummarkers[j] <- dim(markTab.subsubset)[1]
+                marker.tab[[j]] <- table(markTab.subsubset$gene)
+                bin.uniqmarkers[j] <- length(which(marker.tab[[j]] > 0))
+                bin.singlemarkers[j] <- length(which(marker.tab[[j]] ==1))
+            }
         }
     }
     ## Combine SSU Tables ##########################################################
-    if (!is.na(x1$ssuTab) || !is.na(x2$ssuTab)) {
-        ssuTab.add <- unique (rbind(x1$ssuTab,x2$ssuTab))
-        bin.numSSUs <- dim(ssuTab.add)[1]
+    #if (!is.na(x1$ssuTab) || !is.na(x2$ssuTab)) {
+    if (is.data.frame(x1$ssuTab) && is.data.frame(x2$ssuTab)) {
+        if ( dim(x1$ssuTab)[1]>0 || dim(x2$ssuTab)[1]>0 ) {
+            ssuTab.add <- unique (rbind(x1$ssuTab,x2$ssuTab))
+            bin.numSSUs <- dim(ssuTab.add)[1]
+        }
     }
     ## Combine trna Tables #########################################################
-    if (!is.na(x1$trnaTab) || !is.na(x2$trnaTab)) {
-        trnaTab.add <- unique (rbind(x1$trnaTab,x2$trnaTab))
-        bin.numtRNAs <- dim(trnaTab.add)[1]
-        tRNAs.tab <- table(trnaTab.add$tRNA_type)
-        bin.uniqtRNAs <- length(which(tRNAs.tab > 0))
+    #if (!is.na(x1$trnaTab) || !is.na(x2$trnaTab)) {
+    if (is.data.frame(x1$trnaTab) && is.data.frame(x2$trnaTab)) {
+        if ( dim(x1$trnaTab)[1] > 0 || dim(x2$trnaTab)[1] > 0 ) {
+            trnaTab.add <- unique (rbind(x1$trnaTab,x2$trnaTab))
+            bin.numtRNAs <- dim(trnaTab.add)[1]
+            tRNAs.tab <- table(trnaTab.add$tRNA_type)
+            bin.uniqtRNAs <- length(which(tRNAs.tab > 0))
+        }
     }
     ## Summary statistics ##########################################################
     bin.length <- sum(scaff.add$Length)
