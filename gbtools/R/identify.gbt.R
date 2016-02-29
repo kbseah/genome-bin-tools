b36478235d43a09b8b5ff53cc87e19b7bba1660d
kbseah <kbseah@users.noreply.github.com>
2016-02-29 10:54:55 +0100

    Implement identify() for gbt and gbtbin objects

---
+++ gbtools/R/identify.gbt.R
@@ -0,0 +1,57 @@
+#' Identify points in gbtools plot
+#'
+#' Click on GC-coverage plot or differential coverage plot to identify contigs.
+#' In cluttered plots it may not be very accurate! Will write the contig ID
+#' as a label overlay on the plot.
+#'
+#' @return Object of class gbtbin containing identified contigs
+#' @export
+#' @seealso \code{\link{plot.gbt},\link{choosebin}
+
+identify.gbt <- function(d,
+#' @param d Object of class gbt or gbtbin, in plot
+                         slice=1,
+#' @param slice Which sample data was plotted? (Default: 1)
+                         ...
+#' @param ... Further arguments passed to identify.default()
+                         ) {
+    # Catch invalid "slice" parameters
+    if (is.na(slice) || !is.numeric(slice)) {
+        cat ("gbtools ERROR: Please specify valid value for slice parameter\n")
+    } else {
+        # Data frame for GC-coverage plots
+        if (length (slice) == 1) {
+            X <- merge(data.frame(ID=d$scaff$ID,
+                                  Ref_GC=d$scaff$Ref_GC,
+                                  Length=d$scaff$Length,
+                                  Avg_fold=d$scaff$Avg_fold,
+                                  xVals=d$scaff$Ref_GC),
+                       data.frame(ID=d$covs$ID,
+                                  yVals=d$covs[[slice[1]+1]]
+                                  ),
+                       by="ID")
+            names(X) <- c("ID","Ref_GC","Length","Avg_fold","xVals","yVals")
+        }
+        # Data frame for differential coverage plots
+        else if (length(slice) == 2) {
+            X <- merge (data.frame (ID=d$scaff$ID,
+                                    Ref_GC=d$scaff$Ref_GC,
+                                    Length=d$scaff$Length,
+                                    Avg_fold=d$scaff$Avg_fold),
+                        data.frame (ID=d$covs$ID,
+                                    xVals=d$covs[[slice[1]+1]],
+                                    yVals=d$covs[[slice[2]+1]]),
+                        by="ID")
+            names(X) <- c("ID","Ref_GC","Length","Avg_fold","xVals","yVals")
+        }
+        # Implement the identify parameter...
+        shortlist <- identify(x=X$xVals,
+                              y=X$yVals,
+                              labels=X$ID,
+                              ...
+                              )
+        shortlist.contigs <- X$ID[shortlist]
+        return(gbtbin(as.character(shortlist.contigs),d,slice=slice))
+        #return(shortlist)
+    }
+}
---
+++ gbtools/R/identify.gbtbin.R
@@ -0,0 +1,12 @@
+#' Identify points in gbtools plot
+#'
+#' Click on GC-coverage plot or differential coverage plot to identify contigs.
+#' In cluttered plots it may not be very accurate! Will write the contig ID
+#' as a label overlay on the plot.
+#'
+#' @return Object of class gbtbin containing identified contigs
+#' @inheritParams identify.gbt
+#' @export
+#' @seealso \code{\link{plot.gbt},\link{choosebin}
+
+identify.gbtbin <- identify.gbt 
