012f08b09e2af801bf2257a17326a26e49828d3d
kbseah <kbseah@users.noreply.github.com>
2017-01-13 11:17:44 +0100

    Update fastgFish R function with new input var

--- gbtools/R/fastgFish.R
+++ gbtools/R/fastgFish.R
@@ -1,16 +1,17 @@
 #' Perform connectivity fishing with Fastg and paths files from SPAdes 3.6.2+
 #'
 #' Find contigs connected to a given gbtbin and report a new gbtbin object
-#' Requires Fastg and paths files produced by SPAdes 3.6.2+; old version of
+#' Requires Fastg and paths files produced by SPAdes 3.6.2+; older versions of
 #' SPAdes produces different format.
 #'
 #' @param x Object of class gbt (parent object of the gbtbin object)
 #' @param bin Object of class gbtbin, derived from x above
-#' @param fastg.file Fastg formatted assembly graph from SPAdes
-#' @param paths Paths file mapping assembly graph edge names to scaffold/contig names
+#' @param fastg.file Fastg formatted assembly graph from SPAdes (assembly_graph.fastg)
+#' @param paths.file Paths file mapping assembly graph edge names to scaffold/contig names (scaffolds.paths or contigs.paths)
+#' @param fasta.file Fasta file containing scaffolds/contigs of this assembly (scaffolds.fasta or contigs.fasta)
 #' @param save Logical: Save list of fished contigs to external file? (Default: No)
 #' @param file File name to save list of fished contigs, if save=TRUE
 #' @return Object of class gbtbin
 #' @export
 
-fastgFish <- function (x, bin, fastg.file, paths.file, depth, save, file, script.path) UseMethod ("fastgFish") # Define generic for fastgFish function
+fastgFish <- function (x, bin, fastg.file, paths.file, fasta.file, depth, save, file, script.path) UseMethod ("fastgFish") # Define generic for fastgFish function
--- gbtools/R/fastgFish.gbt.R
+++ gbtools/R/fastgFish.gbt.R
@@ -1,13 +1,14 @@
 #' Perform connectivity fishing with Fastg and paths files from SPAdes 3.6.2+
 #'
 #' Find contigs connected to a given gbtbin and report a new gbtbin object
-#' Requires Fastg and paths files produced by SPAdes 3.6.2+; old version of
+#' Requires Fastg and paths files produced by SPAdes 3.6.2+; older versions of
 #' SPAdes produces different format.
 #'
 #' @param x Object of class gbt (parent object of the gbtbin object)
 #' @param bin Object of class gbtbin, derived from x above
-#' @param fastg.file Fastg formatted assembly graph from SPAdes
-#' @param paths Paths file mapping assembly graph edge names to scaffold/contig names
+#' @param fastg.file Fastg formatted assembly graph from SPAdes (assembly_graph.fastg)
+#' @param paths.file Paths file mapping assembly graph edge names to scaffold/contig names (scaffolds.paths or contigs.paths)
+#' @param fasta.file Fasta file containing scaffolds/contigs of this assembly (scaffolds.fasta or contigs.fasta)
 #' @param depth Number of fishing iterations (Default: 0 means iterate until no additional contigs recruited)
 #' @param save Logical: Save list of fished contigs to external file? (Default: No)
 #' @param file File name to save list of fished contigs, if save=TRUE
@@ -19,6 +20,7 @@ fastgFish.gbt <- function (x, # Object of class gbt (parent object of the bin)
                               bin, # Object of class gbtbin
                               fastg.file, # Fastg assembly graph
                               paths.file, # scaffolds.paths or contigs.paths file
+                              fasta.file, # scaffolds.fasta or contigs.fasta file
                               depth=0, # Number of iterations to fish (Default 0 - exhaustive)
                               save=FALSE, # Save result to external file? Default no
                               file="fished_bin.list", # File name to save result
@@ -29,6 +31,7 @@ fastgFish.gbt <- function (x, # Object of class gbt (parent object of the bin)
     command.params <- paste(script.path,
                             "-g", fastg.file,
                             "-p", paths.file,
+                            "-s", fasta.file,
                             "-o /tmp/tmp.fishing_output",
                             "-b -",
                             "-i", depth,
